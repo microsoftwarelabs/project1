1891e02e0b456bec5f84c07a6e221e43463276b9
Stive Hobbys <149022052+start-hobby@users.noreply.github.com>
2023-11-02 12:56:19 -0300

    Create reconhecimento.js

---
+++ reconhecimento.js
@@ -0,0 +1,16 @@
+/*
+Autor: microsoftwarelabs
+numero de contato no zang: https://services.zangi.com/dl/conversation/1094194496
+technical support:https://www.vivaolinux.com.br/~cumestive
+Os direitos autorais desse código pertencem à organização microsoftweralabs.
+The copyright of this Code belongs to the MicrosoftWeralabs organization.
+*/
+
+// Get the current URL
+var url = window.location.href;
+
+// Extract the page name from the URL
+var pageName = url.substring(url.lastIndexOf("/") + 1);
+
+// Output the page name to the console
+console.log(pageName);
