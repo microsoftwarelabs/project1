36a2e22bd2ff1c4f8ce625e5006e13de82e03af4
Matheus <44553903+Populim@users.noreply.github.com>
2023-11-02 12:28:16 -0400

    Added script to generate 2FA codes

---
+++ 2fa.py
@@ -0,0 +1,17 @@
+from hashlib import sha256
+import time
+
+duracao = 30 # 30 segundos
+codigo_secreto = "código secreto"
+
+tempo = time.time()
+tempo_hash = (tempo//duracao)*duracao
+tempo_restante = duracao - (tempo % duracao)
+
+senha = codigo_secreto + str(tempo_hash)
+hash_hex = sha256(senha.encode('utf-8')).hexdigest()
+hash_int = int(hash_hex, 16) # convertendo pra int
+hash_6_digitos = hash_int % 10**6 # pegando os 6 ultimos digitos
+
+print("código de verificação:",hash_6_digitos)
+print("Tempo restante: ", tempo_restante, "segundos")
